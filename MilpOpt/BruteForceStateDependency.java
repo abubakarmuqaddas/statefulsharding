c0eb3fa065dbb899efa4aee33ec8953b7cbf7e4e
Abubakar <abubakar@Abubakar.local>
2017-06-14 02:05:54 +0200

    2017-06-14 0205

---
+++ MilpOpt/BruteForceStateDependency.java
@@ -0,0 +1,34 @@
+package statefulsharding.MilpOpt;
+
+import statefulsharding.graph.ListGraph;
+import statefulsharding.graph.Vertex;
+import statefulsharding.graph.algorithms.ShortestPath;
+import statefulsharding.randomgraphgen.ManhattanGraphGen;
+
+import java.util.HashMap;
+
+/**
+ * Created by abubakar on 14/6/17.
+ */
+public class BruteForceStateDependency {
+
+    public static void main(String[] args) {
+
+        int capacity = Integer.MAX_VALUE;
+
+        int size = 9;
+
+        ManhattanGraphGen manhattanGraphGen = new ManhattanGraphGen(size, capacity,
+                ManhattanGraphGen.mType.UNWRAPPED, false, true);
+        ListGraph graph = manhattanGraphGen.getManhattanGraph();
+
+        HashMap<Vertex, HashMap<Vertex, Integer>> dist = ShortestPath.FloydWarshall(graph, false,
+                null);
+
+
+
+
+
+    }
+
+}
