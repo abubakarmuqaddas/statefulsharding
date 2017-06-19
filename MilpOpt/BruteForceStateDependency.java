4afc8e946b9c933c149211ce88f074fc776d4235
root <abubakar_mcs@live.com>
2017-06-19 19:26:21 +0200

    2017-06-19 1926

--- MilpOpt/BruteForceStateDependency.java
+++ MilpOpt/BruteForceStateDependency.java
@@ -26,7 +26,7 @@ public class BruteForceStateDependency {
          */
 
         int capacity = Integer.MAX_VALUE;
-        int size = 3;
+        int size = 4;
 
         ManhattanGraphGen manhattanGraphGen = new ManhattanGraphGen(size, capacity,
                 ManhattanGraphGen.mType.UNWRAPPED, false, true);
@@ -43,12 +43,13 @@ public class BruteForceStateDependency {
          * Generate states and numCopies
          */
 
-        int dependencySize = 2;
+        int dependencySize = 3;
         StateStore stateStore = new StateStore();
         LinkedList<LinkedList<StateVariable>> allDependencies =
                 GenerateStates.BinaryTreeGenerator(dependencySize, stateStore);
         stateStore.setStateCopies("a",2);
-        stateStore.setStateCopies("b",1);
+        stateStore.setStateCopies("b",2);
+        stateStore.setStateCopies("c",1);
         //stateStore.setStateCopies("c",1);
 
         /*
@@ -106,8 +107,9 @@ public class BruteForceStateDependency {
                 "../Dropbox/PhD_Work/Stateful_SDN/" +
                         "snapsharding/analysis/" +
                         "MANHATTAN-UNWRAPPED_deterministicTfc_optimal_4/" +
-                        "MANHATTAN-UNWRAPPED_deterministicTfc_optimal_4_run_4_traffic.txt"
+                        "MANHATTAN-UNWRAPPED_deterministicTfc_optimal_4_run_2_traffic.txt"
         );
+
 
         for(TrafficDemand trafficDemand : trafficStore.getTrafficDemands()){
             dependencies.put(trafficDemand,
@@ -116,53 +118,83 @@ public class BruteForceStateDependency {
                     )));
         }
 
+        System.out.println();
+        dependencies.forEach((trafficDemand, states) -> {
+            System.out.println(trafficDemand.getSource().getLabel() + " -> " +
+                    trafficDemand.getDestination().getLabel());
+            System.out.print("Var: ");
+            states.forEach(stateVariable -> {
+                System.out.print(stateVariable.getLabel() + " ");
+            });
+            System.out.println();
+            System.out.println();
+        });
+
 
         /*
          * Loop over all combinations!!
          */
 
+        HashMap<LinkedList<StateCopy>, Integer> traffic = new HashMap<>();
 
         LinkedList<StateCopy> bestCombination = null;
+        int x=1;
         int minCombination = Integer.MAX_VALUE;
 
         for (LinkedList<StateCopy> combination : combinations) {
 
+            int combinationTraffic = 0;
             for (TrafficDemand trafficDemand : trafficStore.getTrafficDemands()) {
 
-                
+                System.out.println("Combination: " + x + " Tfc: " + trafficDemand.getSource().getLabel()
+                        + " -> " + trafficDemand.getDestination().getLabel());
 
+                int pathSize = 0;
                 LinkedList<StateVariable> currentDep = dependencies.get(trafficDemand);
+                Vertex currentSrc = trafficDemand.getSource();
 
+                for(int i=0 ; i<currentDep.size() ; i++){
+                StateVariable stateVariable = currentDep.get(i);
 
+                    int minStatePathSize = Integer.MAX_VALUE;
+                    StateCopy minStateCopy = null;
 
+                    for(StateCopy stateCopy : combination){
+                        if(stateCopy.getState().equals(stateVariable)){
+                            Vertex currentDst = stateCopy.getVertex();
+                            int statePathSize = dist.get(currentSrc).get(currentDst);
 
+                            if(statePathSize < minStatePathSize){
+                                minStatePathSize = statePathSize;
+                                minStateCopy = stateCopy;
+                            }
+                        }
+                    }
 
+                    pathSize = pathSize + minStatePathSize;
 
+                    currentSrc = minStateCopy.getVertex();
+                }
 
-
+                Vertex currentDst = trafficDemand.getDestination();
+                pathSize = pathSize + dist.get(currentSrc).get(currentDst);
+                combinationTraffic = combinationTraffic + pathSize;
             }
+            x++;
+            traffic.put(combination, combinationTraffic);
 
 
-
-
-
+            if(combinationTraffic<minCombination){
+                bestCombination = combination;
+                minCombination = combinationTraffic;
+            }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Best combination traffic: " + traffic.get(bestCombination));
+        for (StateCopy stateCopy : bestCombination) {
+            System.out.println(stateCopy.getLabel() + " copy: " + stateCopy.getCopyNumber()
+             + " Vertex: " + stateCopy.getVertex().getLabel());
+        }
 
     }
 
