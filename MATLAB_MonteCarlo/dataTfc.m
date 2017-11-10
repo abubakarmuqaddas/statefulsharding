07a80f59c44d5da7b1c53968f10b048989ae869a
Abubakar <abubakar_mcs@live.com>
2017-11-10 10:09:00 +0100

    2017-09-10 1008

--- MATLAB_MonteCarlo/dataTfc.m
+++ MATLAB_MonteCarlo/dataTfc.m
@@ -39,5 +39,38 @@ for i=1:length(c)
 end
 
 plot(c,mean(dist),'-ok')
-line([0,100],[0.5412,0.5412])
+line([0,max(c)],[0.5412,0.5412])
+hold on
 ylim([0 1])
+
+
+syncDist = zeros(1, length(c));
+
+for iter=1:length(c)
+
+    width=1/sqrt(c(iter));
+
+    xCoords = (0:width:0.99999999)+ width/2;
+    yCoords = (0:width:0.99999999)+ width/2;
+    [X,Y]=meshgrid(xCoords,yCoords);
+    cP=[X(:) Y(:)];
+
+    totalDist=0;
+
+    for i=1:c(iter)
+        for j=1:c(iter)
+
+            if i==j
+                continue;
+            end
+
+            totalDist = totalDist + ...
+                sqrt((cP(i,1)-cP(j,1))^2+(cP(i,2)-cP(j,2))^2);
+        end
+    end
+
+    syncDist(iter)=totalDist/(c(iter)*(c(iter)-1));
+   
+end
+
+plot(c,syncDist,'-*r')
--- MATLAB_MonteCarlo/syncTfc.m
+++ MATLAB_MonteCarlo/syncTfc.m
@@ -4,24 +4,34 @@ clc
 
 
 % Number of copies
-c=2^2;
+c=(2:10).^2;
 
-width=1/sqrt(c(i));
+% Initialize dist matrix
+syncDist = zeros(1, length(c));
 
+for iter=1:length(c)
 
+    width=1/sqrt(c(iter));
 
-for i=1:sqrt(c)
-    
-    for j=1:sqrt(c)
-    
-        if i==j
-            continue;
-              
-    
-    
+    xCoords = (0:width:0.99999999)+ width/2;
+    yCoords = (0:width:0.99999999)+ width/2;
+    [X,Y]=meshgrid(xCoords,yCoords);
+    cP=[X(:) Y(:)];
+
+    totalDist=0;
+
+    for i=1:c(iter)
+        for j=1:c(iter)
+
+            if i==j
+                continue;
+            end
+
+            totalDist = totalDist + ...
+                sqrt((cP(i,1)-cP(j,1))^2+(cP(i,2)-cP(j,2))^2);
+        end
     end
-    
-    
-    
-    
+
+    syncDist(iter)=totalDist/(c(iter)*(c(iter)-1));
+   
 end
